08801e95eb7d8545aa16327c3c2f6c45aa486b01
M.Najjar <M.Najjar@DESKTOP-FH0DTVM.zte.com.cn>
2017-04-17 06:41:12 +0300

    SecondCommit

--- src/backing/OrderManagementInterface.java
+++ src/backing/OrderManagementInterface.java
@@ -8,23 +8,23 @@ public interface OrderManagementInterface {
 	public static final String CUSTOMER_IS_NOT_DELETED = "Customer IS Not Deleted";
 	public static final String EDIT_CUSTOMER = "Customer IS Edit";
 
-	public final String PRODUCT_SUCCESSFUL = "Add Product Successful";
-	public final String ADD_PRODUCT_MSG_ERROR = "Can't Add Product";
-	public final String PRODUCT_DELETED = "Product IS Deleted";
-	public final String PRODUCT_IS_NOT_DELETED = "Product IS Not Deleted";
+	public static final String PRODUCT_SUCCESSFUL = "Add Product Successful";
+	public static final String ADD_PRODUCT_MSG_ERROR = "Can't Add Product";
+	public static final String PRODUCT_DELETED = "Product IS Deleted";
+	public static final String PRODUCT_IS_NOT_DELETED = "Product IS Not Deleted";
 	public static final String EDIT_Product = "Product IS Edit";
 	public static final String DUBLICATE_Product = "Product Is Dublicate";
 
-	public final String ORDER_SUCCESSFUL = "Add ORDER Successful";
-	public final String ADD_ORDER_MSG_ERROR = "Can't Add ORDER";
-	public final String ORDER_DELETED = "ORDER IS Deleted";
-	public final String ORDER_IS_NOT_DELETED = "ORDER IS Not Deleted";
+	public static final String ORDER_SUCCESSFUL = "Add ORDER Successful";
+	public static final String ADD_ORDER_MSG_ERROR = "Can't Add ORDER";
+	public static final String ORDER_DELETED = "ORDER IS Deleted";
+	public static final String ORDER_IS_NOT_DELETED = "ORDER IS Not Deleted";
 	public static final String EDIT_ORDER = "ORDER IS Edit";
 
-	public final String ITEM_SUCCESSFUL = "Add Item Successful";
-	public final String ADD_ITEM_MSG_ERROR = "Can't Add Item";
-	public final String ITEM_DELETED = "Item IS Deleted";
-	public final String ITEM_IS_NOT_DELETED = "Item IS Not Deleted";
+	public static final String ITEM_SUCCESSFUL = "Add Item Successful";
+	public static final String ADD_ITEM_MSG_ERROR = "Can't Add Item";
+	public static final String ITEM_DELETED = "Item IS Deleted";
+	public static final String ITEM_IS_NOT_DELETED = "Item IS Not Deleted";
 	public static final String EDIT_ITEM = "Item IS Edit";
 	public static final String DUBLICATE_ITEM = "Can't Add Item For This Order , Dublicate Item";
 
